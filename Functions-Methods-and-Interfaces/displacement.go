68549069c8f72c1c67c187d92e6d847242ec4553
Kitty Su <kitty.su@coinbase.com>
2020-03-05 14:26:28 -0800

    Fix displacement.go

--- Functions-Methods-and-Interfaces/displacement.go
+++ Functions-Methods-and-Interfaces/displacement.go
@@ -2,14 +2,20 @@ package main
 
 import "fmt"
 
-func GenDisplaceFn(a, v0, s0 float64) func(float64) float64 {
+func GenDisplaceFn(a float64, v float64, s float64) func(float64) float64 {
 	return func(t float64) float64 {
-		return 1/2*a*t*t+v0*t+s0
+		fmt.Println( a)
+		fmt.Println( v)
+		fmt.Println( s)
+		return 0.5*a*t*t+v*t+s
 	}
 }
 
 func main() {
-	var a, v0, s0, t float64
+	var a float64
+	var v0 float64
+	var s0 float64
+	var t float64
 
 	fmt.Print("Enter acceleration: ")
 	fmt.Scan(&a)
@@ -18,11 +24,14 @@ func main() {
 	fmt.Scan(&v0)
 
 	fmt.Print("Enter initial displacement: ")
-	fmt.Scan(&a)
+	fmt.Scan(&s0)
 
 	fn := GenDisplaceFn(a, v0, s0)
 
 	fmt.Print("Enter time: ")
 	fmt.Scan(&t)
+
 	fmt.Printf("Total displacement is %v. \n", fn(t))
 }
+
+
